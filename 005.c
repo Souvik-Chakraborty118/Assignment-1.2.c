d32e9b53d3ff7ca38aa981c9bddba00dd80364ce
Sauvik666 <hueiw892@gmail.com>
2025-12-01 11:57:07 +0530

    Sum of numbers using Sentinel

---
+++ 005.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+int n,sum=0;
+const int seni=-1;
+printf("Enter numbers to sum and type %d to stop\n\n",seni);
+printf("Enter a number: ");
+scanf("%d",&n);
+while(n!=seni){
+    sum=sum+n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+}
+printf("\nSum of all digits: %d",sum);
+return 0;
+}
